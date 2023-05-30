71dfa54f335d806c41b8ad5bb8eebb46d568f0b3
Lauren Slayman <laurenaslayman@gmail.com>
2023-05-30 13:58:20 -0600

    passes tests, adds notes

--- server/app.py
+++ server/app.py
@@ -8,7 +8,7 @@ from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
-from models import db, Activity, Camper
+from models import db, Activity, Camper, Signup
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
@@ -22,6 +22,79 @@ db.init_app(app)
 @app.route('/')
 def home():
     return ''
+
+# GET /campers
+# Return JSON data in the format below. Note: you should return a JSON response in this format, without any additional nested data related to each camper.
+@app.route('/campers', methods=['GET', 'POST'])
+def campers():
+    if request.method == 'GET':
+        return [camper.to_dict() for camper in Camper.query.all()]
+
+# POST /campers
+# Return JSON data in the format below. Note: you should return a JSON response in this format, without any additional nested data related to each camper.    
+# If the Camper is created successfully, send back a response with the new Camper
+# If the Camper is not created successfully, return the following JSON data, along with the appropriate HTTP status code:    
+    elif request.method == 'POST':
+        data = request.get_json()
+
+        try:
+            camper = Camper(name=data.get('name'), age = data.get('age'))
+            db.session.add(camper)
+            db.session.commit()
+            return camper.to_dict()
+        except ValueError:
+            return{'error': '400: Validation Error'}, 400
+
+# GET /campers/int:id
+# If the Camper exists, return JSON data in the format below. Note: you will need to serialize the data for this response differently than for the GET /campers route. Make sure to include an array of activities for each camper.        
+@app.route('/campers/<int:id>', methods=['GET'])
+def camper_by_id(id):
+    camper = Camper.query.filter_by(id=id).first()
+
+    if camper:
+        return camper.to_dict()
+    return {"error": "404: Camper not found"}, 404
+
+# GET /activities
+# Return JSON data
+@app.route('/activities', methods=['GET'])
+def activities():
+    #because using to_dict, create serializer rules in models.py
+    return [activity.to_dict() for activity in Activity.query.all()]
+
+# DELETE /activities/int:id
+# If the Activity exists, it should be removed from the database, along with any Signups that are associated with it (a Signup belongs to an Activity, so you need to delete the Signups before the Activity can be deleted).
+# After deleting the Activity, return an empty response body, along with the appropriate HTTP status code.
+# If the Activity does not exist, return the following JSON data, along with the appropriate HTTP status code:
+@app.route('/activities/<int:id>', methods=['DELETE'])
+def activity_by_id(id):
+    activity = Activity.query.filter_by(id=id).first()
+
+    if activity:
+        db.session.delete(activity)
+        db.session.commit()
+        return{}, 204
+    return{"error": "404: Activity not found"}, 404
+
+# POST /signups
+# This route should create a new Signup that is associated with an existing Camper and Activity. It should accept an object with the following properties in the body of the request:
+# If the Signup is created successfully, send back a response with the data related to the Activity:
+# If the Signup is not created successfully, return the following JSON data, along with the appropriate HTTP status code:
+@app.route('/signups', methods=['POST'])
+def signups():
+    data = request.get_json()
+
+    try:
+        signup = Signup(
+            time = data.get('time'),
+            camper_id = data.get('camper_id'),
+            activity_id = data.get('activity_id')
+        )
+        db.session.add(signup)
+        db.session.commit()
+        return signup.to_dict()
+    except ValueError:
+        return {"error": "400: Validation Error"}, 400
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -19,9 +19,16 @@ db = SQLAlchemy(metadata=metadata)
 class Activity(db.Model, SerializerMixin):
     __tablename__ = 'activities'
 
+    # Serialize rules needed for GET request for Activities in app.py
+    serialize_rules = ('-campers.activities', '-signups.activity')
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     difficulty = db.Column(db.Integer)
+
+    # An Activity has many Signups and has many Campers *through* Signups
+    signups = db.relationship('Signup', backref='activity')
+    campers = association_proxy('signups', 'camper', creator=lambda cmp: Signup(camper=cmp))
 
     def __repr__(self):
         return f'<Activity {self.id}: {self.name}>'
@@ -29,9 +36,31 @@ class Activity(db.Model, SerializerMixin):
 class Camper(db.Model, SerializerMixin):
     __tablename__ = 'campers'
 
+    # Needed for GET request on app.py
+    serialize_rules = ('-activities.campers', '-signups.camper')
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     age = db.Column(db.Integer)
+
+    # A Camper has many Signups, and have many Activity(s) *through* Signups
+    signups = db.relationship("Signup", backref='camper')
+    activities = association_proxy('signups', 'activity', creator=lambda act: Signup(activity=act))
+
+    # Add validations to the Camper model:
+    # must have a name
+    # must have an age between 8 and 18
+    @validates('name')
+    def validates_name(self, key, name):
+        if name:
+            return name
+        raise ValueError("Camper must have a name.")
+
+    @validates('age')
+    def validates_age(self, key, age):
+        if 8 <= age <= 18:
+            return age
+        raise ValueError("Camper must be between 8 and 18 years-old.")
 
     def __repr__(self):
         return f'<Camper {self.id}: {self.name}>'
@@ -39,7 +68,24 @@ class Camper(db.Model, SerializerMixin):
 class Signup(db.Model, SerializerMixin):
     __tablename__ = 'signups'
 
+    # Needed for POST request in app.py
+    serialize_rules = ('-activity.signups', '-camper.signups')
+
     id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.Integer)
+
+    # A Signup belongs to a Camper and belongs to an Activity
+    camper_id = db.Column(db.Integer, db.ForeignKey('campers.id'))
+    activity_id = db.Column(db.Integer, db.ForeignKey('activities.id'))
+
+    # Add validations to the Signup model:
+    # must have a time between 0 and 23 (referring to the hour of day for the activity)
+    @validates('time')
+    def validate_time(self, key, time):
+        if 0 <= time <= 23:
+            return time
+        raise ValueError("Time must be an integer between 0 and 23")
+
 
     def __repr__(self):
         return f'<Signup {self.id}>'
